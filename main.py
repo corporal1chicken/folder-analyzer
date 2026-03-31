1649d7539408c2f0a0ba257f34039e47c084dbe2
corporal1chicken <omar.y.ahmed26@gmail.com>
2026-03-31 12:31:29 +0100

    Code Refactor

--- main.py
+++ main.py
@@ -2,49 +2,69 @@ from pathlib import Path
 import os
 import sys
 
+current_files = []
+
+def validate_drive(drive):
+    if len(drive) != 1:
+        print("Drive must be exactly 1 letter")
+        return False
+
+    if not os.path.exists(f"{drive}:/"):
+        print("Drive does not exist")
+        return False
+    
+    return True
+
+def validate_folder(path):
+    if path.is_file():
+        print(f"{path} is a file, not a folder")
+        return False
+
+    if not path.exists():
+        print(f"{path} does not exist")
+        return False
+    
+    return True
+
+def add_files(path):
+    files = []
+
+    for root, folders, file_list in os.walk(path):
+        for file in file_list:
+            name, extension = os.path.splitext(file)
+
+            files.append({"name": name, "extension": extension})
+
+    return files
+
 drive = input("Enter the drive to use. Must be 1 letter: ").upper()
+drive_valid = validate_drive(drive)
 
-if len(drive) != 1:
-    print("Drive cannot be more than 1 letter")
-    sys.exit()
-
-if not os.path.exists(f"{drive}:/"):
-    print("Drive does not exist")
+if not drive_valid:
     sys.exit()
 
 folder_name = input(f"Enter a folder name in drive {drive}: ")
-folder_path = Path(drive + ":/" + folder_name)
+folder_path = Path(f"{drive}:/{folder_name}")
 
-if folder_path.is_file():
-    print(f"{folder_name} is a file, not a folder")
+folder_valid = validate_folder(folder_path)
+
+if not folder_valid:
     sys.exit()
-
-if not os.path.exists(drive + ":/" + folder_name):
-    print(f"{folder_name} does not exist in drive {drive}")
-    sys.exit()
-
-file_names = []
-file_extensions = []
-
-for root, folders, files in os.walk(folder_path):
-    for file in files:
-        name, extension = os.path.splitext(file)
-
-        file_names.append(name)
-        file_extensions.append(extension)
+else:
+    current_files = add_files(folder_path)
 
 options = ["1", "2", "3"]
-option = input("(1) Print files names\n(2) Print file extentions\n(3) Print both\nEnter: ")
+option = input("(1) Print file names\n(2) Print file extensions\n(3) Print both\nEnter: ")
 
 if option not in options:
     print(f"{option} is not a valid option")
     sys.exit()
 elif option == "1":
-    for f in file_names:
-        print(f)
+    for f in current_files:
+        print(f["name"])
 elif option == "2":
-    for f in file_extensions:
-        print(f)
+    for f in current_files:
+        print(f["extension"])
 elif option == "3":
-    for name, ext in zip(file_names, file_extensions):
-        print(f"Name: {name} | Ext: {ext}")
+    for f in current_files:
+        print(f"Name: {f["name"]} | Ext: {f["extension"]}")

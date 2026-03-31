b42dd4d8762e5f289b9f3d52b015f8c82a6c4bed
corporal1chicken <omar.y.ahmed26@gmail.com>
2026-03-31 19:50:25 +0100

    Report and More Metadata

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from pathlib import Path
 import os
 import sys
+import datetime
+import math
+
+types = {
+    "image": [".png", ".jpg"],
+    "video": [".mp4"],
+    "text": [".txt", ".json", ".csv"]
+}
 
 def validate_drive(drive):
     if len(drive) != 1:
@@ -24,16 +32,74 @@ def validate_folder(path):
     
     return True
 
-def add_files(path):
+def add_files(text_path):
     files = []
+    windows_path = Path(text_path)
 
-    for root, folders, file_list in os.walk(path):
+    for root, folders, file_list in os.walk(windows_path):
         for file in file_list:
             name, extension = os.path.splitext(file)
 
-            files.append({"name": name, "extension": extension})
+            extra_info = os.stat(text_path + "/" + file)
+
+            size = extra_info.st_size
+            last_mod = datetime.datetime.fromtimestamp(extra_info.st_mtime)
+            
+            file_type = "Unknown"
+            for f_type, allowed_ext in types.items():
+                if extension in allowed_ext:
+                    file_type = f_type
+
+            files.append(
+                {
+                    "name": name, 
+                    "extension": extension,
+                    "size": size,
+                    "last_modified": last_mod,
+                    "type": file_type
+                }
+            )
 
     return files
+
+def format_size(bytes, decimal = 2):
+    if bytes == 0:
+        return "0 Bytes"
+    
+    power = 1024
+    units = ["Bytes", "KB", "MB", "GB"]
+
+    i = int(math.floor(math.log(bytes, power)))
+    format = f"{bytes / (power ** i):.{decimal}f} {units[i]}"
+
+    return format
+
+def generate_report(files, folder_path):
+    file_count = {}
+
+    print("-----------------------------------")
+    print(f"FOLDER PATH: {folder_path}")
+    print("-----------------------------------")
+
+    for f in files:
+        print(f"Name: {f["name"]} | Ext: {f["extension"]}")
+        print(f"Type: {f["type"].capitalize()}")
+        print(f"Raw: {f["size"]} Bytes | Size: {format_size(f["size"])}")
+        print(f"Last Modified: {f["last_modified"]}")
+        print("-----------------------------------")
+
+        if file_count.get(f["type"]):
+            file_count[f["type"]] += 1
+        else:
+            file_count[f["type"]] = 1
+
+    print(f"There are {sum(file_count.values())} files")
+
+    for t, c in file_count.items():
+        print(f"{c} {t} files")
+
+    print("-----------------------------------")
+
 
 drive = input("Enter the drive to use. Must be 1 letter: ").upper()
 drive_valid = validate_drive(drive)
@@ -42,54 +108,13 @@ if not drive_valid:
     sys.exit()
 
 folder_name = input(f"Enter a folder name in drive {drive}: ")
-folder_path = Path(f"{drive}:/{folder_name}")
+folder_path = f"{drive}:/{folder_name}"
 
-folder_valid = validate_folder(folder_path)
+folder_valid = validate_folder(Path(folder_path))
 
 if not folder_valid:
     sys.exit()
 else:
     current_files = add_files(folder_path)
 
-types = {
-    "image": [".png", ".jpg"],
-    "video": [".mp4"],
-    "text": [".txt", ".json", ".csv"]
-}
-
-options = ["1", "2", "3", "4"]
-option = input("(1) Print file names\n(2) Print file extensions\n(3) Print both\n(4) Get file count\nEnter: ")
-
-if option not in options:
-    print(f"{option} is not a valid option")
-    sys.exit()
-elif option == "1":
-    for f in current_files:
-        print(f["name"])
-elif option == "2":
-    for f in current_files:
-        print(f["extension"])
-elif option == "3":
-    for f in current_files:
-        print(f"Name: {f["name"]} | Ext: {f["extension"]}")
-elif option == "4":
-    chosen_type = input("Pick a type to count (image, text, video): ").lower()
-
-    if chosen_type not in types.keys():
-        print(f"{chosen_type} is not a valid type")
-        sys.exit()
-
-    file_count = {}
-    for f in current_files:
-        extension = f["extension"]
-
-        if extension in types[chosen_type]:
-            if file_count.get(extension):
-                file_count[extension] += 1
-            else:
-                file_count[extension] = 1
-
-    print(f"There are {sum(file_count.values())} {chosen_type} files in {folder_path}")
-    print("Breakdown:")
-    for ext, count in file_count.items():
-        print(f"Ext: {ext} | {count}")
+generate_report(current_files, folder_path)
